d95ac8fd0a469061dd100b9bceea893465d68d52
Anushka Jaiswal <nush1@Anushkas-MacBook-Pro.local>
2026-07-02 18:04:16 +0530

    Complete CLI CRUD commands

--- src/main.cpp
+++ src/main.cpp
@@ -45,6 +45,47 @@ else if (command == "GET")
 
     std::cout << db.get(key) << std::endl;
 }
+else if (command == "EXISTS")
+{
+    std::string key;
+
+    ss >> key;
+
+    if (db.exists(key))
+    {
+        std::cout << "TRUE" << std::endl;
+    }
+    else
+    {
+        std::cout << "FALSE" << std::endl;
+    }
+}
+else if (command == "DEL")
+{
+    std::string key;
+
+    ss >> key;
+
+    if (db.del(key))
+    {
+        std::cout << "OK" << std::endl;
+    }
+    else
+    {
+        std::cout << "Key not found" << std::endl;
+    }
+}
+else if (command == "SIZE")
+{
+    std::cout << db.size() << std::endl;
+}
+else if (command == "CLEAR")
+{
+    db.clear();
+
+    std::cout << "OK" << std::endl;
+}
+
     }
 
     std::cout << "Goodbye!" << std::endl;
